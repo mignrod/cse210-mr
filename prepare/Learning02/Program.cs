e121205de84dd6f7e94dc9541a2e55b423224268
mignrod <mignrod@gmail.com>
2024-03-04 16:04:08 -0400

    learning02 finished

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Cellphone Technician";
+        job1._company = "JL Solutions";
+        job1._startYear = 2017;
+        job1._endYear = 2023;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Manager";
+        job2._company = "DS Telecomunications";
+        job2._startYear = 2010;
+        job2._endYear = 2017;
+
+        Resume resume = new Resume();
+        resume._personName = "Miguelangel Rodriguez";
+
+        resume._jobs.Add(job1);
+        resume._jobs.Add(job2);
+
+        resume.DisplayResume();
+
     }
 }
